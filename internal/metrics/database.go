c02565dd5932854a978612b7600c0a4bad8c3118
Chris Marslender <chrismarslender@gmail.com>
2023-02-03 12:43:43 -0600

    Add timestamp column to blocks table

--- internal/metrics/database.go
+++ internal/metrics/database.go
@@ -4,6 +4,7 @@ package metrics
 func (m *Metrics) initTables() error {
 	query := "CREATE TABLE IF NOT EXISTS `blocks` (" +
 		"  `id` int unsigned NOT NULL AUTO_INCREMENT," +
+		"  `timestamp` DATETIME DEFAULT NULL," +
 		"  `height` int DEFAULT NULL," +
 		"  `farmer_puzzle_hash` varchar(255) DEFAULT NULL," +
 		"  `farmer_address` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL," +
